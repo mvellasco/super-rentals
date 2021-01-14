821e70499ada28c71c41ce4f0f3dce5d6180c74b
Miguel Vellasco <miguelvellasco@gmail.com>
2021-01-14 15:59:16 -0300

    feat(rental): added image component with presentation logic

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function(hooks) {
@@ -18,5 +18,28 @@ module('Integration | Component | rental/image', function(hooks) {
     assertTomster.exists();
     assertTomster.hasAttribute('src', '/assets/images/teaching-tomster.png');
     assertTomster.hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('it toggles its size when clicked', async function(assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('button.image').exists();
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
+
+    await click('button.image');
+
+    assert.dom('.image').hasClass('large');
+    assert.dom('.image small').hasText('View Smaller');
+
+    await click('button.image');
+
+    assert.dom('.image').doesNotHaveClass('large');
+    assert.dom('.image small').hasText('View Larger');
   });
 });
